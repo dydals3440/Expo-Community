f4e63e4c30000019f9cc0fad623e5a828cdfa9e5
김용민 <dydals3440@gmail.com>
2025-02-27 22:13:10 +0900

    feat: 회원가입 / 로그인 검증 로직을 추가합니다.

--- components/EmailInput.tsx
+++ components/EmailInput.tsx
@@ -7,12 +7,23 @@ function EmailInput() {
     <Controller
       name="email"
       control={control}
-      render={({ field: { onChange, value } }) => (
+      rules={{
+        validate: (data: string) => {
+          if (data.length === 0) {
+            return "이메일을 입력해주세요.";
+          }
+          if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(data)) {
+            return "올바른 이메일 형식이 아닙니다.";
+          }
+        },
+      }}
+      render={({ field: { onChange, value }, fieldState: { error } }) => (
         <InputField
           label="이메일"
           placeholder="이메일을 입력해주세요."
           onChangeText={onChange}
           value={value}
+          error={error?.message}
         />
       )}
     />
--- components/PasswordInput.tsx
+++ components/PasswordInput.tsx
@@ -7,12 +7,24 @@ function PasswordInput() {
     <Controller
       name="password"
       control={control}
-      render={({ field: { onChange, value } }) => (
+      rules={{
+        validate: (data: string) => {
+          if (data.length === 0) {
+            return "비밀번호를 입력해주세요.";
+          }
+          if (data.length < 8) {
+            return "비밀번호는 8자리 이상이어야 합니다.";
+          }
+        },
+      }}
+      render={({ field: { onChange, value }, fieldState: { error } }) => (
         <InputField
           label="비밀번호"
           placeholder="비밀번호을 입력해주세요."
           onChangeText={onChange}
+          secureTextEntry
           value={value}
+          error={error?.message}
         />
       )}
     />
