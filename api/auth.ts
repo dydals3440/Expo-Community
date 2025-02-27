01b78d0afd278d2676c6d350d69de7675a7633c0
김용민 <dydals3440@gmail.com>
2025-02-27 22:54:59 +0900

    feat: 로그인 API 만들기

---
+++ api/auth.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { getSecureStore } from "@/utils/secureStore";
+import axiosInstance from "@/api/axios";
+
+type RequestUser = {
+  email: string;
+  password: string;
+};
+
+async function postSignup(body: RequestUser): Promise<void> {
+  const { data } = await axiosInstance.post("/auth/signup", body);
+
+  return data;
+}
+
+async function postLogin(body: RequestUser): Promise<{ accessToken: string }> {
+  const { data } = await axiosInstance.post("/auth/signin", body);
+
+  return data;
+}
+
+async function getMe() {
+  const accessToken = await getSecureStore("accessToken");
+  const { data } = await axiosInstance.get("/auth/me", {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  return data;
+}
+
+export { postSignup, postLogin, getMe };
